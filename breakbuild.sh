cf36ea827b5fe421b860c78dc2188ea21505edef
Alberto Cantalejo Sancho <70366199+acantalejo-keepler@users.noreply.github.com>
2021-03-30 12:14:10 +0200

    Update breakbuild.sh

--- breakbuild.sh
+++ breakbuild.sh
@@ -17,6 +17,7 @@ CE_TASK_ID_KEY="ceTaskId="
 
 SONAR_INSTANCE="${1}"
 SONAR_ACCESS_TOKEN="${2}"
+SONAR_LOGIN="${3}"
 SLEEP_TIME=5
 
 echo "QG Script --> Using SonarQube instance ${SONAR_INSTANCE}"
@@ -38,7 +39,7 @@ wait_for_success=true
 
 while [ "${wait_for_success}" = "true" ]
 do
-  ce_status=$(curl -s -u "${SONAR_ACCESS_TOKEN}": "${SONAR_INSTANCE}"/api/ce/task?id=${ce_task_id} | jq -r .task.status)
+  ce_status=$(curl -s -u "${SONAR_LOGIN}:${SONAR_ACCESS_TOKEN}": "${SONAR_INSTANCE}"/api/ce/task?id=${ce_task_id} | jq -r .task.status)
 
   echo "QG Script --> Status of SonarQube task is ${ce_status}"
 
@@ -60,11 +61,11 @@ do
 
 done
 
-ce_analysis_id=$(curl -s -u $SONAR_ACCESS_TOKEN: $SONAR_INSTANCE/api/ce/task?id=$ce_task_id | jq -r .task.analysisId)
+ce_analysis_id=$(curl -s -u $SONAR_LOGIN:$SONAR_ACCESS_TOKEN: $SONAR_INSTANCE/api/ce/task?id=$ce_task_id | jq -r .task.analysisId)
 echo "QG Script --> Using analysis id of ${ce_analysis_id}"
 
 # get the status of the quality gate for this analysisId
-qg_status=$(curl -s -u $SONAR_ACCESS_TOKEN: $SONAR_INSTANCE/api/qualitygates/project_status?analysisId="${ce_analysis_id}" | jq -r .projectStatus.status)
+qg_status=$(curl -s -u $SONAR_LOGIN:$SONAR_ACCESS_TOKEN: $SONAR_INSTANCE/api/qualitygates/project_status?analysisId="${ce_analysis_id}" | jq -r .projectStatus.status)
 echo "QG Script --> Quality Gate status is ${qg_status}"
 
 if [ "${qg_status}" != "OK" ]; then
