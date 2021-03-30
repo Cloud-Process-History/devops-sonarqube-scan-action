3d408df054027f5690c77c2fb81c7b1dca70afb9
Alberto Cantalejo Sancho <70366199+acantalejo-keepler@users.noreply.github.com>
2021-03-30 12:51:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ sonar-scanner \
 	-Dsonar.host.url=${INPUT_HOST} \
     	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
     	-Dsonar.login=${INPUT_LOGIN} \
-    	-Dsonar.password=${INPUT_PASSWORD}
+    	-Dsonar.password=${INPUT_PASSWORD} \
+	-Dsonar.qualitygate.wait \
+	-Dsonar.qualitygate.timeout=300
 
-echo "------------------"
-echo " RUN QUALITY GATE "
-echo "------------------"
-pwd
-ls
-sh /breakbuild.sh ${INPUT_HOST} ${INPUT_PASSWORD} ${INPUT_LOGIN} 
+#echo "------------------"
+#echo " RUN QUALITY GATE "
+#echo "------------------"
+#pwd
+#ls
+#sh /breakbuild.sh ${INPUT_HOST} ${INPUT_PASSWORD} ${INPUT_LOGIN} 
