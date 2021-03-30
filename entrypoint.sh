45ddf55985ba0673553e5dae638750adb90adde0
Alberto Cantalejo Sancho <70366199+acantalejo-keepler@users.noreply.github.com>
2021-03-30 13:07:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-sonar-scanner \
-	-Dsonar.host.url=${INPUT_HOST} \
-    	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
-    	-Dsonar.login=${INPUT_LOGIN} \
-    	-Dsonar.password=${INPUT_PASSWORD} \
-	-Dsonar.qualitygate.wait \
-	-Dsonar.qualitygate.timeout=300
+if [[ "${INPUT_CHECKQUALITYGATE}" == "true" ]]; then
+	sonar-scanner \
+		-Dsonar.host.url=${INPUT_HOST} \
+    		-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+    		-Dsonar.login=${INPUT_LOGIN} \
+    		-Dsonar.password=${INPUT_PASSWORD} \
+		-Dsonar.qualitygate.wait \
+		-Dsonar.qualitygate.timeout=300
+else
+	sonar-scanner \
+		-Dsonar.host.url=${INPUT_HOST} \
+    		-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+    		-Dsonar.login=${INPUT_LOGIN} \
+    		-Dsonar.password=${INPUT_PASSWORD}
+fi
